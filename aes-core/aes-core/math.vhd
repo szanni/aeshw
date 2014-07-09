70e73b453fa28d6bafc8abaa80c5e255a411d993
Angelo Haller <angelo@szanni.org>
2014-07-09 18:50:57 +0200

    Add functions to muliply by 0x9 0xb 0xd 0xe in GF(2^8).

--- aes-core/aes-core/math.vhd
+++ aes-core/aes-core/math.vhd
@@ -4,41 +4,105 @@ use work.types.all;
 
 package math is
 
-	function mul2(d_in : byte) return byte;
-	function mul3(d_in : byte) return byte;
+	function mul2(din : byte) return byte;
+	function mul3(din : byte) return byte;
+	function mul9(din : byte) return byte;
+	function mulb(din : byte) return byte;
+	function muld(din : byte) return byte;
+	function mule(din : byte) return byte;
 
 end math;
 
 package body math is
 
-	function mul2(d_in : byte) return byte is
+	function mul2(din : byte) return byte is
 		variable ret : byte;
 	begin
-		ret(0) := d_in(7);
-		ret(1) := d_in(0) xor d_in(7);
-		ret(2) := d_in(1);
-		ret(3) := d_in(2) xor d_in(7);
-		ret(4) := d_in(3) xor d_in(7);
-		ret(5) := d_in(4);
-		ret(6) := d_in(5);
-		ret(7) := d_in(6);
+		ret(0) := din(7);
+		ret(1) := din(0) xor din(7);
+		ret(2) := din(1);
+		ret(3) := din(2) xor din(7);
+		ret(4) := din(3) xor din(7);
+		ret(5) := din(4);
+		ret(6) := din(5);
+		ret(7) := din(6);
 
 		return ret;
 	end mul2;
 
-	function mul3(d_in : byte) return byte is
+	function mul3(din : byte) return byte is
 		variable ret : byte;
 	begin
-		ret(0) := d_in(0) xor d_in(7);
-		ret(1) := d_in(0) xor d_in(1) xor d_in(7);
-		ret(2) := d_in(1) xor d_in(2);
-		ret(3) := d_in(2) xor d_in(3) xor d_in(7);
-		ret(4) := d_in(3) xor d_in(4) xor d_in(7);
-		ret(5) := d_in(4) xor d_in(5);
-		ret(6) := d_in(5) xor d_in(6);
-		ret(7) := d_in(6) xor d_in(7);
+		ret(0) := din(0) xor din(7);
+		ret(1) := din(0) xor din(1) xor din(7);
+		ret(2) := din(1) xor din(2);
+		ret(3) := din(2) xor din(3) xor din(7);
+		ret(4) := din(3) xor din(4) xor din(7);
+		ret(5) := din(4) xor din(5);
+		ret(6) := din(5) xor din(6);
+		ret(7) := din(6) xor din(7);
 
 		return ret;
 	end mul3;
+
+	function mul9(din : byte) return byte is
+		variable ret : byte;
+	begin
+		ret(0) := din(0) xor din(5);
+		ret(1) := din(1) xor din(5) xor din(6);
+		ret(2) := din(2) xor din(6) xor din(7);
+		ret(3) := din(0) xor din(3) xor din(5) xor din(7);
+		ret(4) := din(1) xor din(4) xor din(5) xor din(6);
+		ret(5) := din(2) xor din(5) xor din(6) xor din(7);
+		ret(6) := din(3) xor din(6) xor din(7);
+		ret(7) := din(4) xor din(7);
+
+		return ret;
+	end mul9;
+
+	function mulb(din : byte) return byte is
+		variable ret : byte;
+	begin
+		ret(0) := din(0) xor din(5) xor din(7);
+		ret(1) := din(0) xor din(1) xor din(5) xor din(6) xor din(7);
+		ret(2) := din(1) xor din(2) xor din(6) xor din(7);
+		ret(3) := din(0) xor din(2) xor din(3) xor din(5);
+		ret(4) := din(1) xor din(3) xor din(4) xor din(5) xor din(6) xor din(7);
+		ret(5) := din(2) xor din(4) xor din(5) xor din(6) xor din(7);
+		ret(6) := din(3) xor din(5) xor din(6) xor din(7);
+		ret(7) := din(4) xor din(6) xor din(7);
+
+		return ret;
+	end mulb;
+
+	function muld(din : byte) return byte is
+		variable ret : byte;
+	begin
+		ret(0) := din(0) xor din(5) xor din(6);
+		ret(1) := din(1) xor din(5) xor din(7);
+		ret(2) := din(0) xor din(2) xor din(6);
+		ret(3) := din(0) xor din(1) xor din(3) xor din(5) xor din(6) xor din(7);
+		ret(4) := din(1) xor din(2) xor din(4) xor din(5) xor din(7);
+		ret(5) := din(2) xor din(3) xor din(5) xor din(6);
+		ret(6) := din(3) xor din(4) xor din(6) xor din(7);
+		ret(7) := din(4) xor din(5) xor din(7);
+
+		return ret;
+	end muld;
+
+	function mule(din : byte) return byte is
+		variable ret : byte;
+	begin
+		ret(0) := din(5) xor din(6) xor din(7);
+		ret(1) := din(0) xor din(5);
+		ret(2) := din(0) xor din(1) xor din(6);
+		ret(3) := din(0) xor din(1) xor din(2) xor din(5) xor din(6);
+		ret(4) := din(1) xor din(2) xor din(3) xor din(5);
+		ret(5) := din(2) xor din(3) xor din(4) xor din(6);
+		ret(6) := din(3) xor din(4) xor din(5) xor din(7);
+		ret(7) := din(4) xor din(5) xor din(6);
+
+		return ret;
+	end mule;
 
 end math;
