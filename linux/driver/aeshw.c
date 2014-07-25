8ee8a1d1dbe85e7e06995402367ce04fc2846995
Angelo Haller <angelo@szanni.org>
2014-07-25 07:39:17 +0200

    Add cbc mode to kernel driver.

--- linux/driver/aeshw.c
+++ linux/driver/aeshw.c
@@ -6,31 +6,52 @@
 #include <asm/io.h>
 #include <linux/of_platform.h>
 
-#define OFFSET_DIN  0x0
-#define OFFSET_DOUT 0x10
-#define OFFSET_CTL  0x20
-#define OFFSET_STAT 0x24
+enum AES_MODE {
+	AES_MODE_ENCRYPT = 0x00000000,
+	AES_MODE_DECRYPT = 0x01010101,
+	AES_MODE_SET_KEY = 0x02020202
+};
+
+enum OFFSET {
+	OFFSET_DIN  = 0x0,
+	OFFSET_DOUT = 0x10,
+	OFFSET_CTL  = 0x20,
+	OFFSET_STAT = 0x24,
+};
 
 unsigned long remap_size;
 unsigned long phys_addr;
 unsigned long *virt_addr;
 
-struct aeshw_ctx {
-};
-
-static int aeshw_ecb_setkey(struct crypto_tfm *tfm, const u8 *in_key,
-			    unsigned int key_len)
+void aeshw_write(const void *src, enum AES_MODE mode)
 {
-	u32 mode = 0x02020202;
-
-	//printk(KERN_INFO "Set ecb key in aeshw!\n");
-
-	memcpy_toio((u8*)virt_addr + OFFSET_DIN, in_key, key_len);
-
+	memcpy_toio((u8*)virt_addr + OFFSET_DIN, src, AES_BLOCK_SIZE);
 	iowrite32(mode, (u8*)virt_addr + OFFSET_CTL);
+
 	while (ioread32((u8*)virt_addr + OFFSET_STAT) == 0) {
 		//idle
 	}
+}
+
+void aeshw_xor_inplace(const void *dst, const void *src)
+{
+	int i;
+
+	for (i = 0; i < 4; ++i)
+		*((u32*)dst + i) ^= *((u32*)src + i);
+}
+
+void aeshw_read(void *dst)
+{
+	memcpy_fromio(dst, (u8*)virt_addr + OFFSET_DOUT, AES_BLOCK_SIZE);
+}
+
+static int aeshw_setkey(struct crypto_tfm *tfm, const u8 *in_key,
+			    unsigned int key_len)
+{
+	printk(KERN_INFO "Set key in aeshw!\n");
+
+	aeshw_write(in_key, AES_MODE_SET_KEY);
 
 	return 0;
 }
@@ -40,26 +61,18 @@ static int aeshw_ecb_encrypt(struct blkcipher_desc *desc,
 			     unsigned int nbytes)
 {
 	int rv;
-	u32 mode = 0x0;
 	struct blkcipher_walk walk;
 
-	//printk(KERN_INFO "Encrypt ecb in aeshw!\n");
+	printk(KERN_INFO "Encrypt ecb in aeshw!\n");
 
 	blkcipher_walk_init(&walk, dst, src, nbytes);
 	rv = blkcipher_walk_virt(desc, &walk);
 
-	while ((nbytes = walk.nbytes)) {
-		memcpy_toio((u8*)virt_addr + OFFSET_DIN, walk.src.virt.addr, AES_BLOCK_SIZE);
-		iowrite32(mode, (u8*)virt_addr + OFFSET_CTL);
+	while ((walk.nbytes)) {
+		aeshw_write(walk.src.virt.addr, AES_MODE_ENCRYPT);
+		aeshw_read(walk.dst.virt.addr);
 
-		while (ioread32((u8*)virt_addr + OFFSET_STAT) == 0) {
-			//idle
-		}
-
-		memcpy_fromio(walk.dst.virt.addr, (u8*)virt_addr + OFFSET_DOUT, AES_BLOCK_SIZE);
-
-		nbytes -= AES_BLOCK_SIZE;
-		rv = blkcipher_walk_done(desc, &walk, nbytes);
+		rv = blkcipher_walk_done(desc, &walk, walk.nbytes - AES_BLOCK_SIZE);
 	}
 
 	return rv;
@@ -70,26 +83,18 @@ static int aeshw_ecb_decrypt(struct blkcipher_desc *desc,
 			     unsigned int nbytes)
 {
 	int rv;
-	u32 mode = 0x01010101;
 	struct blkcipher_walk walk;
 
-	//printk(KERN_INFO "Decrypt ecb in aeshw!\n");
+	printk(KERN_INFO "Decrypt ecb in aeshw!\n");
 
 	blkcipher_walk_init(&walk, dst, src, nbytes);
 	rv = blkcipher_walk_virt(desc, &walk);
 
-	while ((nbytes = walk.nbytes)) {
-		memcpy_toio((u8*)virt_addr + OFFSET_DIN, walk.src.virt.addr, AES_BLOCK_SIZE);
-		iowrite32(mode, (u8*)virt_addr + OFFSET_CTL);
+	while ((walk.nbytes)) {
+		aeshw_write(walk.src.virt.addr, AES_MODE_DECRYPT);
+		aeshw_read(walk.dst.virt.addr);
 
-		while (ioread32((u8*)virt_addr + OFFSET_STAT) == 0) {
-			//idle
-		}
-
-		memcpy_fromio(walk.dst.virt.addr, (u8*)virt_addr + OFFSET_DOUT, AES_BLOCK_SIZE);
-
-		nbytes -= AES_BLOCK_SIZE;
-		rv = blkcipher_walk_done(desc, &walk, nbytes);
+		rv = blkcipher_walk_done(desc, &walk, walk.nbytes - AES_BLOCK_SIZE);
 	}
 
 	return rv;
@@ -101,20 +106,88 @@ static struct crypto_alg aeshw_ecb_alg = {
 	.cra_priority		=	100,
 	.cra_flags		=	CRYPTO_ALG_TYPE_BLKCIPHER,
 	.cra_blocksize		=	AES_BLOCK_SIZE,
-	.cra_ctxsize		=	sizeof(struct aeshw_ctx),
-	.cra_alignmask		=	0,
 	.cra_type		=	&crypto_blkcipher_type,
 	.cra_module		=	THIS_MODULE,
 	.cra_u			=	{
 		.blkcipher = {
 			.min_keysize		=	AES_KEYSIZE_128,
 			.max_keysize		=	AES_KEYSIZE_128,
-			.setkey	   		= 	aeshw_ecb_setkey,
+			.setkey	   		= 	aeshw_setkey,
 			.encrypt		=	aeshw_ecb_encrypt,
 			.decrypt		=	aeshw_ecb_decrypt,
 		}
 	}
 };
+
+static int aeshw_cbc_encrypt(struct blkcipher_desc *desc,
+			     struct scatterlist *dst, struct scatterlist *src,
+			     unsigned int nbytes)
+{
+
+	int rv;
+	struct blkcipher_walk walk;
+
+	printk(KERN_INFO "Encrypt cbc in aeshw!\n");
+
+	blkcipher_walk_init(&walk, dst, src, nbytes);
+	rv = blkcipher_walk_virt_block(desc, &walk, AES_BLOCK_SIZE);
+
+	while ((walk.nbytes)) {
+		aeshw_xor_inplace(walk.src.virt.addr, walk.iv);
+		aeshw_write(walk.src.virt.addr, AES_MODE_ENCRYPT);
+		aeshw_read(walk.dst.virt.addr);
+		memcpy(walk.iv, walk.dst.virt.addr, AES_BLOCK_SIZE);
+
+		rv = blkcipher_walk_done(desc, &walk, walk.nbytes - AES_BLOCK_SIZE);
+	}
+
+	return rv;
+}
+
+static int aeshw_cbc_decrypt(struct blkcipher_desc *desc,
+			     struct scatterlist *dst, struct scatterlist *src,
+			     unsigned int nbytes)
+{
+	int rv;
+	struct blkcipher_walk walk;
+
+	printk(KERN_INFO "Decrypt cbc in aeshw!\n");
+
+	blkcipher_walk_init(&walk, dst, src, nbytes);
+	rv = blkcipher_walk_virt(desc, &walk);
+
+	while ((walk.nbytes)) {
+		aeshw_write(walk.src.virt.addr, AES_MODE_DECRYPT);
+		aeshw_read(walk.dst.virt.addr);
+		aeshw_xor_inplace(walk.dst.virt.addr, walk.iv);
+		memcpy(walk.iv, walk.src.virt.addr, AES_BLOCK_SIZE);
+
+		rv = blkcipher_walk_done(desc, &walk, walk.nbytes - AES_BLOCK_SIZE);
+	}
+
+	return rv;
+}
+
+static struct crypto_alg aeshw_cbc_alg = {
+	.cra_name		=	"cbc(aes)",
+	.cra_driver_name	=	"aeshw-cbc",
+	.cra_priority		=	100,
+	.cra_flags		=	CRYPTO_ALG_TYPE_BLKCIPHER,
+	.cra_blocksize		=	AES_BLOCK_SIZE,
+	.cra_type		=	&crypto_blkcipher_type,
+	.cra_module		=	THIS_MODULE,
+	.cra_u			=	{
+		.blkcipher = {
+			.min_keysize		=	AES_KEYSIZE_128,
+			.max_keysize		=	AES_KEYSIZE_128,
+			.ivsize	   		= 	AES_BLOCK_SIZE,
+			.setkey	   		= 	aeshw_setkey,
+			.encrypt		=	aeshw_cbc_encrypt,
+			.decrypt		=	aeshw_cbc_decrypt,
+		}
+	}
+};
+
 
 static int aeshw_probe(struct platform_device *pdev)
 {
@@ -124,11 +197,14 @@ static int aeshw_probe(struct platform_device *pdev)
 	if ((rv = crypto_register_alg(&aeshw_ecb_alg)))
 		goto err;
 
+	if ((rv = crypto_register_alg(&aeshw_cbc_alg)))
+		goto err_unregister_ecb;
+
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	if (res == NULL) {
 		dev_err(&pdev->dev, "No memory resource\n");
 		rv = -ENODEV;
-		goto err_unregister_ecb;
+		goto err_unregister_cbc;
 	}
 
 	remap_size = res->end - res->start + 1;
@@ -137,7 +213,7 @@ static int aeshw_probe(struct platform_device *pdev)
 	if (request_mem_region(phys_addr, remap_size, pdev->name) == NULL) {
 		dev_err(&pdev->dev, "Cannot request IO\n");
 		rv = -ENXIO;
-		goto err_unregister_ecb;
+		goto err_unregister_cbc;
 	}
 
 	virt_addr = ioremap_nocache(phys_addr, remap_size);
@@ -154,6 +230,9 @@ static int aeshw_probe(struct platform_device *pdev)
 err_release_mem:
 	release_mem_region(phys_addr, remap_size);
 
+err_unregister_cbc:
+	crypto_unregister_alg(&aeshw_cbc_alg);
+
 err_unregister_ecb:
 	crypto_unregister_alg(&aeshw_ecb_alg);
 
@@ -166,6 +245,7 @@ static int aeshw_remove(struct platform_device *pdev)
 {
 	iounmap(virt_addr);
 	release_mem_region(phys_addr, remap_size);
+	crypto_unregister_alg(&aeshw_cbc_alg);
 	crypto_unregister_alg(&aeshw_ecb_alg);
 	printk(KERN_INFO "Removed aeshw device!\n");
 	return 0;
